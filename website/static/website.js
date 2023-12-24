ce8e01752efea30ec3d18223a1f999038f045fe0
TheMatrix2 <ermolovich.boris@gmail.com>
2023-12-25 00:33:25 +0300

    Deleted useless files

--- website/static/website.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-      $('table tbody tr').click(function() {
-        var clientId = $(this).data('id');
-        window.location.href = '/client/' + clientId;
-      });
-    });
