dd3a59bcae48443b75bc76ae5b4573725e71a80b
TheMatrix2 <ermolovich.boris@gmail.com>
2023-12-25 00:34:28 +0300

    Added Course model

--- website/models.py
+++ website/models.py
@@ -2,28 +2,26 @@ from django.db import models
 
 
 class Client(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    # created_by = models.CharField(max_length=100, auto_created=True)
     first_name = models.CharField(max_length=50)
     middle_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=100)
-    phone = models.CharField(max_length=15)
+    phone = models.CharField(max_length=15, null=True)
     city = models.CharField(max_length=50)
     state = models.CharField(max_length=50)
+    course_id_1 = models.IntegerField(default=0)
+    course_id_2 = models.IntegerField(default=0)
+    course_id_3 = models.IntegerField(default=0)
 
     def __str__(self):
-        return (f"{self.first_name} {self.middle_name} {self.last_name}")
+        return (f"{self.middle_name} {self.first_name} {self.last_name}")
 
 
-# class User(models.Model):
-#     is_superuser = models.SmallIntegerField()
-#     date_joined = models.DateTimeField()
-#     username = models.CharField(max_length=150)
-#     first_name = models.CharField(max_length=50)
-#     middle_name = models.CharField(max_length=50)
-#     last_name = models.CharField(max_length=50)
-#     email = models.EmailField(max_length=100)
-#
-#     def __str__(self):
-#         return (f'{self.username} #{self.id}')
+class Course(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    name_of_teacher = models.CharField(max_length=100)
+    number_of_students = models.IntegerField(default=0)
+
+    def __str__(self):
+        return (f"{self.title} ({self.name_of_teacher})")
