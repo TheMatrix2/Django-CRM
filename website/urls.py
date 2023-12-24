a6f2b42b15a8f35e7c8996ed837586d8663c2cfc
TheMatrix2 <ermolovich.boris@gmail.com>
2023-12-25 00:34:59 +0300

    Added endpoints for pages about courses

--- website/urls.py
+++ website/urls.py
@@ -7,9 +7,19 @@ urlpatterns = [
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register_user, name='register'),
+
     path('add-client/', views.add_client, name='add-client'),
-    path('client/<int:id>', views.view_client, name='client'),
-    path('edit-client/<int:id>', views.edit_client, name='edit-client'),
-    path('delete-client/<int:id>', views.delete_client, name='delete-client'),
-    path('clients/', views.view_clients, name='clients')
+    path('client/<int:client_id>', views.view_client, name='client'),
+    path('clients/', views.view_clients, name='clients'),
+    path('edit-client/<int:client_id>', views.edit_client, name='edit-client'),
+    path('delete-client/<int:client_id>', views.delete_client, name='delete-client'),
+
+    path('add-course/', views.add_course, name='add-course'),
+    path('course/<int:course_id>', views.view_course, name='course'),
+    path('courses/', views.view_courses, name='courses'),
+    path('edit-course/<int:course_id>', views.edit_course, name='edit-course'),
+    path('delete-course/<int:course_id>', views.delete_course, name='delete-course'),
+
+    path('course/<int:course_id>/add-student', views.add_student, name='add-student'),
+    path('delete-student/<int:course_id>/<int:student_id>', views.delete_student, name='delete-student'),
 ]
